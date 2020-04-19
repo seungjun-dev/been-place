2d978a9e9c78f2bf6e58f6cae5e8a214969d241e
seungjun-dev <seungjun91@naver.com>
2020-04-20 08:21:12 +0900

    background pic name

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
 const body = document.querySelector("body");
 const NUMBER_OF_PICS = 3;
 
+const backgroundPics = {
+    1: {name: 'Cityscape of Jamsil, Korea', src: './image/1.jpg'},
+    2: {name: 'Hanok Village Seoul, Korea', src: './image/2.jpg'},
+    3: {name: 'Night View Seoul, Korea', src: './image/3.jpg'}
+}
+
 function setBackgroundImage(number) {
     const image = new Image();
     const bgDiv = document.getElementById('background');
+    const bgName = document.getElementById('bgName');
 
-    image.src = `./image/${number + 1}.jpg`;
+    //image.src = `./image/${number + 1}.jpg`;
+    image.src = backgroundPics[number+1].src;
     image.classList.add("bg");
 
     bgDiv.prepend(image);
+    bgName.innerText = backgroundPics[number+1].name;
 }
 
 function getRandomNumber() {
