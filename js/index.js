a5f6b03e3645e69b46142e0c99df5658521fdb85
seungjun-dev <seungjun91@naver.com>
2020-04-20 08:29:14 +0900

    background descriptions

--- js/index.js
+++ js/index.js
@@ -2,15 +2,16 @@ const body = document.querySelector("body");
 const NUMBER_OF_PICS = 3;
 
 const backgroundPics = {
-    1: {name: 'Cityscape of Jamsil, Korea', src: './image/1.jpg'},
-    2: {name: 'Hanok Village Seoul, Korea', src: './image/2.jpg'},
-    3: {name: 'Night View Seoul, Korea', src: './image/3.jpg'}
+    1: {name: 'Cityscape of Jamsil, Korea', src: './image/1.jpg', author: 'Sunyu Kim', ref: 'Unsplash'},
+    2: {name: 'Hanok Village Seoul, Korea', src: './image/2.jpg', author: 'Yeo Khee', ref: 'Unsplash'},
+    3: {name: 'Night View Seoul, Korea', src: './image/3.jpg', author: 'Yohan Cho', ref: 'Unsplash'}
 }
 
 function setBackgroundImage(number) {
     const image = new Image();
     const bgDiv = document.getElementById('background');
     const bgName = document.getElementById('bgName');
+    const bgDesc = document.getElementById('bgDesc');
 
     //image.src = `./image/${number + 1}.jpg`;
     image.src = backgroundPics[number+1].src;
@@ -18,6 +19,7 @@ function setBackgroundImage(number) {
 
     bgDiv.prepend(image);
     bgName.innerText = backgroundPics[number+1].name;
+    bgDesc.innerText = "Photo by " + backgroundPics[number+1].author + " " + backgroundPics[number+1].ref;
 }
 
 function getRandomNumber() {
